9a9c0e20712d891b9539eb2a5d4cbae533a97d01
Zhu Qiwen <qiwenzhu@ciilabs.org>
2014-11-13 21:47:43 -0500

    Add data

--- ABR_Final/src/hello/Helloword.java
+++ ABR_Final/src/hello/Helloword.java
@@ -4,5 +4,6 @@ import weka.classifiers.*;
 public class Helloword {
 	public static void main(String args[]) {
 		System.out.println("hello world!");
+		System.out.println("This is chiven's print");
 	}
 }
