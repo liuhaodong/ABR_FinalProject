bdaf5e11ae8e1028e65c6418858adb8b891c8f63
Haodong Liu <haodongliu@Haodongs-MBP.wv.cc.cmu.edu>
2014-11-13 22:45:32 -0500

    Add test nb classifier

--- ABR_Final/src/classifiers/NaiveBayes.java
+++
@@ -1,5 +0,0 @@
-package classifiers;
-
-public class NaiveBayes {
-
-}
---
+++ ABR_Final/src/hello/my_nb.java
@@ -0,0 +1,29 @@
+package hello;
+
+import weka.classifiers.bayes.NaiveBayes;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+
+
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+
+public class my_nb {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader bReader = new BufferedReader(new FileReader(
+				"data/mushroom_train.arff"));
+		Instances train = new Instances(bReader);
+		train.setClassIndex(0);
+		bReader.close();
+		
+		NaiveBayes nb = new NaiveBayes();
+		nb.buildClassifier(train);
+		Evaluation eval = new Evaluation(train);
+		eval.crossValidateModel(nb, train, 10, new Random(1));
+		System.out.println(eval.toSummaryString());
+	}
+
+}
