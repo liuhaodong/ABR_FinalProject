b7f4402d6c1005766679aa65c322727d5143fe94
Haodong Liu <haodongliu@Haodongs-MBP.wv.cc.cmu.edu>
2014-11-15 20:24:04 -0500

    Add data preprocessing

---
+++ ABR_Final/src/dataPreprocessing/FeatureRemove.java
@@ -0,0 +1,52 @@
+package dataPreprocessing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FeatureRemove {
+	public void randomRemoveFeature(String inputFile, String outputFile,
+			float percentage) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
+
+		String tmpLine = "";
+		while ((tmpLine = br.readLine()) != null) {
+			if (tmpLine.startsWith("@data")) {
+				bw.write(tmpLine);
+				bw.write('\n');
+				int tmpChar = 0;
+				while ((tmpChar = br.read()) != -1) {
+					if (tmpChar == ',' || tmpChar == '\n') {
+						bw.write(tmpChar);
+						if (tmpChar == '\n') {
+							if ((tmpChar = br.read()) != -1)
+								bw.write(tmpChar);
+						}
+					} else {
+						double randomNum = Math.random();
+						if (randomNum < percentage) {
+							bw.write('?');
+						} else {
+							bw.write(tmpChar);
+						}
+					}
+				}
+			} else {
+				bw.write(tmpLine);
+				bw.write('\n');
+			}
+		}
+		br.close();
+		bw.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		FeatureRemove tmpFeatureRemove = new FeatureRemove();
+		tmpFeatureRemove.randomRemoveFeature("data/mushroom_train.arff",
+				"data/mushroom_train_miss.arff", (float) 0.9);
+	}
+
+}
--- ABR_Final/src/hello/my_nb.java
+++ ABR_Final/src/hello/my_nb.java
@@ -14,7 +14,7 @@ public class my_nb {
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader bReader = new BufferedReader(new FileReader(
-				"data/mushroom_train.arff"));
+				"data/mushroom_train_miss.arff"));
 		Instances train = new Instances(bReader);
 		train.setClassIndex(0);
 		bReader.close();
@@ -25,5 +25,5 @@ public class my_nb {
 		eval.crossValidateModel(nb, train, 10, new Random(1));
 		System.out.println(eval.toSummaryString());
 	}
-
+	
 }
